05ca18287804a6fa707e45214223a753388a8ace
Ishaan Thakur <ishaant99@gmail.com>
2022-03-17 17:29:32 -0300

    Dicts

--- 5_dicts.py
+++ 5_dicts.py
@@ -1,5 +1,4 @@
 # DICTONARIES IN PYTHON
-
 thisDict = {
     'brand': 'mercedes',
     'model': 'A-class',
@@ -15,3 +14,19 @@ k = thisDict.keys()
 v = thisDict.values()
 
 print(thisDict)
+
+print(sorted(thisDict))
+
+# del thisDict['year']
+
+print(thisDict)
+
+for x in thisDict:
+    print(x, thisDict[x])
+
+for x,y in thisDict.items():
+    print(x,y)
+
+mydict = thisDict.copy()
+
+print(mydict)
